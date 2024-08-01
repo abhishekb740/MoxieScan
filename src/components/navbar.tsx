33caa265bae416d8b39213687a27706a1f2b60a6
Abhishek <abhishekb740@gmail.com>
2024-08-02 01:04:38 +0530

    style: font changed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 
 const Navbar = () => {
     return (
-        <div className="flex flex-row px-2 py-4 rounded-lg justify-center">
+        <div className="flex flex-row px-2 py-4 rounded-lg justify-center font-rubik">
             <div className="text-2xl font-bold">
                 MoxieScan
             </div>
