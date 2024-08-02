ee1ad0ff4bcd98c0175d3093764f8de389e25836
Neel <neelp0980@gmail.com>
2024-08-02 17:21:20 +0530

    fix: data caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,41 @@
-"use client";
-import Hero from "@/components/home";
 import Navbar from "@/components/navbar";
+import dynamic from "next/dynamic";
+import { fetchAuctionsWithBids } from "@/app/_actions/queries";
 
-export default function Home() {
-  
-  return (
-    <main className="flex min-h-screen flex-col">
-      <div>
-        <div className="m-4">
-          <Navbar />
+export const fetchCache = "force-no-store";
+
+const Hero = dynamic(() => import("@/components/home"), {
+  loading: () => (
+    <div className="flex bg-opacity-50 z-50 justify-center items-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+  ),
+});
+
+export default async function Home() {
+  try {
+    const data = await fetchAuctionsWithBids();
+    data.sort((a, b) => b.timestamp - a.timestamp);
+    return (
+      <main className="flex min-h-screen flex-col">
+        <div>
+          <div className="m-4">
+            <Navbar />
+          </div>
+          <Hero bids={data as Bid[]} totalBids={data.length} />
         </div>
-        <Hero />
-      </div>
-    </main>
-  );
+      </main>
+    );
+  } catch {
+    return (
+      <main className="flex min-h-screen flex-col">
+        <div>
+          <div className="m-4">
+            <Navbar />
+          </div>
+          <p className="text-white font-medium">Failed to fetch bids...</p>
+        </div>
+      </main>
+    );
+  }
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,35 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { fetchAuctionsWithBids } from "@/app/_actions/queries";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { formatDistanceToNow } from "date-fns";
 
 const PAGE_SIZE = 10;
 
-const Hero = () => {
-  const [bids, setBids] = useState<Bid[]>([]);
+type HeroProps = {
+  bids: Bid[];
+  totalBids: number;
+};
+
+const Hero = ({ bids, totalBids }: HeroProps) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [totalBids, setTotalBids] = useState<number>(0);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const getBids = async () => {
-      try {
-        const data = await fetchAuctionsWithBids();
-        data.sort((a, b) => b.timestamp - a.timestamp);
-        setBids(data);
-        setTotalBids(data.length);
-        setLoading(false);
-      } catch (e) {
-        console.error("Failed to fetch bids", e);
-        setError("Failed to fetch bids");
-        setLoading(false);
-      }
-    };
-
-    getBids();
-  }, []);
 
   const formatRelativeTime = (timestamp: number) => {
     return formatDistanceToNow(new Date(timestamp * 1000), { addSuffix: true });
@@ -44,53 +27,112 @@ const Hero = () => {
     currentPage * PAGE_SIZE
   );
 
-  if (loading) return (
-    <div className="flex bg-opacity-50 z-50 justify-center items-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
-  );
-
   return (
     <div className="px-20 font-rubik">
       <div>
-        <table className="min-w-full divide-y divide-gray-200 bg-black text-white" style={{ borderCollapse: 'separate', borderSpacing: '0' }}>
+        <table
+          className="min-w-full divide-y divide-gray-200 bg-black text-white"
+          style={{ borderCollapse: "separate", borderSpacing: "0" }}
+        >
           <thead className="">
             <tr className="bg-purple-700 text-md">
-              <th className="px-6 py-3 text-white tracking-wider text-left" style={{ borderTopLeftRadius: '0.75rem', borderBottomLeftRadius: '0.75rem' }}>User</th>
-              <th className="px-6 py-3 text-white tracking-wider text-left">Moxie</th>
-              <th className="px-6 py-3 text-white tracking-wider text-left">Bid</th>
-              <th className="px-6 py-3 text-white tracking-wider text-left">Current Price</th>
-              <th className="px-6 py-3 text-white tracking-wider text-left" style={{ borderTopRightRadius: '0.75rem', borderBottomRightRadius: '0.75rem' }}>Time</th>
+              <th
+                className="px-6 py-3 text-white tracking-wider text-left"
+                style={{
+                  borderTopLeftRadius: "0.75rem",
+                  borderBottomLeftRadius: "0.75rem",
+                }}
+              >
+                User
+              </th>
+              <th className="px-6 py-3 text-white tracking-wider text-left">
+                Moxie
+              </th>
+              <th className="px-6 py-3 text-white tracking-wider text-left">
+                Bid
+              </th>
+              <th className="px-6 py-3 text-white tracking-wider text-left">
+                Current Price
+              </th>
+              <th
+                className="px-6 py-3 text-white tracking-wider text-left"
+                style={{
+                  borderTopRightRadius: "0.75rem",
+                  borderBottomRightRadius: "0.75rem",
+                }}
+              >
+                Time
+              </th>
             </tr>
           </thead>
           <tbody className="bg-black divide-y divide-gray-700">
             {paginatedBids.map((bid, index) => (
               <tr key={bid.encodedOrderId}>
-                <td className="px-6 py-4 whitespace-nowrap flex flex-row gap-2 items-center" style={index === paginatedBids.length - 1 ? { borderBottomLeftRadius: '0.75rem' } : {}}>
+                <td
+                  className="px-6 py-4 whitespace-nowrap flex flex-row gap-2 items-center"
+                  style={
+                    index === paginatedBids.length - 1
+                      ? { borderBottomLeftRadius: "0.75rem" }
+                      : {}
+                  }
+                >
                   <div>
-                    {bid.profileImage ? <img className='w-8 h-8 rounded-full' src={bid.profileImage ?? ''} alt="profile Image" /> :
+                    {bid.profileImage ? (
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src={bid.profileImage ?? ""}
+                        alt="profile Image"
+                      />
+                    ) : (
                       <div className="bg-gradient-to-b from-violet-500 to-blue-600 w-8 h-8 rounded-full shadow-lg"></div>
-                    }
+                    )}
                   </div>
-                  {bid.profileName === null ? bid.user.address.slice(0, 5) + "..." + bid.user.address.slice(bid.user.address.length - 4, bid.user.address.length) : bid.profileName}
+                  {bid.profileName === null
+                    ? bid.user.address.slice(0, 5) +
+                      "..." +
+                      bid.user.address.slice(
+                        bid.user.address.length - 4,
+                        bid.user.address.length
+                      )
+                    : bid.profileName}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap">{Number(bid.volume).toLocaleString()} Moxie</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {Number(bid.volume).toLocaleString()} Moxie
+                </td>
                 <td className="px-6 py-4 whitespace-nowrap flex flex-row gap-2 items-center">
-                  <div className="text-[#F7BF6A]">
-                    bids on
-                  </div>
+                  <div className="text-[#F7BF6A]">bids on</div>
                   <div>
-                    {
-                      bid.tokenProfileImage ?
-                        <img className='w-8 h-8 rounded-full' src={bid.tokenProfileImage ?? ''} alt="token profile Image" />
-                        :
-                        <div className="bg-gradient-to-b from-violet-500 to-blue-600 w-8 h-8 rounded-full shadow-lg"></div>
-                    }
+                    {bid.tokenProfileImage ? (
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src={bid.tokenProfileImage ?? ""}
+                        alt="token profile Image"
+                      />
+                    ) : (
+                      <div className="bg-gradient-to-b from-violet-500 to-blue-600 w-8 h-8 rounded-full shadow-lg"></div>
+                    )}
                   </div>
-                  {bid.tokenProfileName ?? bid.auctioningToken?.slice(0, 5) + "..." + bid.auctioningToken?.slice(bid.auctioningToken?.length - 4, bid.auctioningToken?.length)}
+                  {bid.tokenProfileName ??
+                    bid.auctioningToken?.slice(0, 5) +
+                      "..." +
+                      bid.auctioningToken?.slice(
+                        bid.auctioningToken?.length - 4,
+                        bid.auctioningToken?.length
+                      )}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap">{Number(bid.price).toLocaleString()}$</td>
-                <td className="px-6 py-4 whitespace-nowrap" style={index === paginatedBids.length - 1 ? { borderBottomRightRadius: '0.75rem' } : {}}>{formatRelativeTime(Number(bid.timestamp))}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {Number(bid.price).toLocaleString()}$
+                </td>
+                <td
+                  className="px-6 py-4 whitespace-nowrap"
+                  style={
+                    index === paginatedBids.length - 1
+                      ? { borderBottomRightRadius: "0.75rem" }
+                      : {}
+                  }
+                >
+                  {formatRelativeTime(Number(bid.timestamp))}
+                </td>
               </tr>
             ))}
           </tbody>
